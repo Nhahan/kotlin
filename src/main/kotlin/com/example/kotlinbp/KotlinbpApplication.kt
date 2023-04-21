f6fde17331affe7683dcc2e0d072e3b91f429b52
Nhahan <kisy324@naver.com>
2023-04-21 18:42:28 +0900

    first commit

---
+++ src/main/kotlin/com/example/kotlinbp/KotlinbpApplication.kt
@@ -0,0 +1,11 @@
+package com.example.kotlinbp
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class KotlinbpApplication
+
+fun main(args: Array<String>) {
+    runApplication<KotlinbpApplication>(*args)
+}
