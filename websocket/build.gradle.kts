86a7f03f31ed48bb16bce5016e256d5e5b74bb0e
Nhahan <kisy324@naver.com>
2023-12-17 20:31:42 +0900

    websocket config

--- websocket/build.gradle.kts
+++ websocket/build.gradle.kts
@@ -20,6 +20,7 @@ repositories {
 
 dependencies {
     implementation("org.springframework.boot:spring-boot-starter-websocket")
+    implementation("io.github.oshai:kotlin-logging-jvm:5.1.0")
     implementation("org.jetbrains.kotlin:kotlin-reflect")
     testImplementation("org.springframework.boot:spring-boot-starter-test")
 }
