04a5da7eb3864be0032afa901a8e2a2a4a51a2a0
Nhahan <kisy324@naver.com>
2023-12-22 14:29:21 +0900

    @AuthToken to get Jwt from Header

---
+++ reactive-auth/src/main/kotlin/com/auth/config/resolver/AuthTokenResolver.kt
@@ -0,0 +1,31 @@
+package com.auth.config.resolver
+
+import com.auth.config.annotation.AuthToken
+import org.springframework.core.MethodParameter
+import org.springframework.stereotype.Component
+import org.springframework.web.reactive.BindingContext
+import org.springframework.web.reactive.result.method.HandlerMethodArgumentResolver
+import org.springframework.web.server.ServerWebExchange
+import reactor.core.publisher.Mono
+import reactor.kotlin.core.publisher.toMono
+
+@Component
+class AuthTokenResolver : HandlerMethodArgumentResolver {
+    override fun supportsParameter(parameter: MethodParameter): Boolean {
+        return parameter.hasParameterAnnotation(AuthToken::class.java)
+    }
+
+    override fun resolveArgument(
+        parameter: MethodParameter,
+        bindingContext: BindingContext,
+        exchange: ServerWebExchange
+    ): Mono<Any> {
+
+        val authHeader = exchange.request.headers["Authorization"]?.first()
+        checkNotNull(authHeader)
+
+        val token = authHeader.split(" ")[1]
+        return token.toMono()
+    }
+
+}
