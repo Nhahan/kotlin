efcc2049d669a920dcd9de96f36a04c5c60d1c35
Nhahan <kisy324@naver.com>
2023-12-25 23:55:25 +0900

    BaseEntity & EnableAuditing

--- mvc-example/issue-service/src/main/kotlin/com/mvc/issueservice/IssueServiceApplication.kt
+++ mvc-example/issue-service/src/main/kotlin/com/mvc/issueservice/IssueServiceApplication.kt
@@ -4,12 +4,15 @@ import mu.KLogger
 import mu.KotlinLogging
 import org.springframework.boot.autoconfigure.SpringBootApplication
 import org.springframework.boot.runApplication
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing
 
 @SpringBootApplication
+@EnableJpaAuditing
 class IssueServiceApplication
 fun <R : Any> R.log(): KLogger {
 	return KotlinLogging.logger(this::class.qualifiedName!!)
 }
+
 fun main(args: Array<String>) {
 	runApplication<IssueServiceApplication>(*args)
 }
