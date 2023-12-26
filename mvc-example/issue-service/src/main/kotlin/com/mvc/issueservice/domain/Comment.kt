e56b30f6c389880aca69ee4fe2096e7ea13939e0
Nhahan <kisy324@naver.com>
2023-12-26 15:08:23 +0900

    domains: Table & Repository

---
+++ mvc-example/issue-service/src/main/kotlin/com/mvc/issueservice/domain/Comment.kt
@@ -0,0 +1,21 @@
+package com.mvc.issueservice.domain
+
+import jakarta.persistence.Entity
+import jakarta.persistence.FetchType
+import jakarta.persistence.GeneratedValue
+import jakarta.persistence.GenerationType
+import jakarta.persistence.Id
+import jakarta.persistence.JoinColumn
+import jakarta.persistence.ManyToOne
+
+@Entity
+class Comment(
+    @Id @GeneratedValue(strategy = GenerationType.IDENTITY)
+    val id: Long? = null,
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "issue_id")
+    val issue: Issue,
+    val userId: Long,
+    val username: String,
+    var body: String,
+) : BaseEntity()
---
+++ mvc-example/issue-service/src/main/kotlin/com/mvc/issueservice/domain/IssueRepository.kt
@@ -0,0 +1,8 @@
+package com.mvc.issueservice.domain
+
+import org.springframework.data.jpa.repository.JpaRepository
+
+interface IssueRepository : JpaRepository<Issue, Long> {
+
+    fun findAllByStatusOrderByCreatedAtDesc(status: IssueStatus) : List<Issue>?
+}
